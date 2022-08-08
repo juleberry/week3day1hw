cbbc2fd0838c42c578146132e4a7a6e2ad07a46d
juleberry <105573843+juleberry@users.noreply.github.com>
2022-08-08 13:31:37 -0400

    create person class

--- homework.js
+++ homework.js
@@ -7,10 +7,62 @@ class Hamster {
   wheelRun() {
     console.log('squeak squeak');
   }
+
   eatFood() {
     console.log('nibble nibble');
   }
+
   getPrice() {
     return this.price;
+  }
+}
+
+class Person {
+  constructor(name, age, height, weight, mood, hamsters, bankAccount) {
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamsters = [];
+    this.bankAccount = 0;
+  }
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+  
+  getWeight() {
+    return this.weight;
+  }
+
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  }
+
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+
+  exercise() {
+    this.weight--;
+  }
+
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount += 10;
+  }
+
+  buyHamster(hamster) {
+    this.hamsters.push([]);
+    this.mood += 10;
+    this.bankAccount - Hamster.getPrice(hamster);
   }
 }
